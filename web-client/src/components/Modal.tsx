62dcd7c2a67e6b9297e008f605d27856195ee9bf
Julian <163280302+JEstermann@users.noreply.github.com>
2026-07-23 21:55:09 +0200

    Fix Modal

--- web-client/src/components/Modal.tsx
+++ web-client/src/components/Modal.tsx
@@ -36,7 +36,7 @@ useEffect(() => {
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 p-4 backdrop-blur-sm"
+      className="relative inset-0 z-50 flex items-center justify-center bg-black/60 p-4 backdrop-blur-sm"
       onClick={onClose}
     >
       <div
